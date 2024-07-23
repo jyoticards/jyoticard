bfa567e861d7c623ac4dbbc1b3a3c40bb26f57fa
jyoticards <sourabhagrawal386@gmail.com>
2024-07-23 22:08:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ if item_no:
         st.markdown(f'<p class="result">Matching Items: {a}, {b}, {c}</p>', unsafe_allow_html=True)
 
     # Display image
-    image_path = os.path.join('ITEM IMAGES', f'{item_no}.jpeg')  # Adjust the file extension as needed
+    image_path = {item_no}.jpeg # Adjust the file extension as needed
     if os.path.exists(image_path):
         st.image(image_path, caption=f'Image of {item_no}', use_column_width=True)
     else:
